d485dd62ff68fa2fd4741083680a975400e1cde9
Sanket <sanketbhondge123@gmail.com>
2021-08-18 16:20:41 +0530

    "second commit"

--- myjavaapp/src/myjavaapp/HelloWorld.java
+++ myjavaapp/src/myjavaapp/HelloWorld.java
@@ -1,10 +1,15 @@
 package myjavaapp;
 
 public class HelloWorld {
+	
+	public static int sum(int a,int b) {
+		return a+b;
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hello world");
+		System.out.println(sum(6,5));
 	}
 
 }
